f381c554cc3187ece98f7a17297956b1416257f8
aasoto <andresalfredosotosuarez@gmail.com>
2023-03-24 14:42:48 -0500

    refactor: fields in select city renamed appropiately

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -204,8 +204,8 @@ const submit = () => {
 
                 <select
                     class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-                    name="state"
-                    id="state"
+                    name="city"
+                    id="city"
                     v-model="form.city"
                     required
                 >
