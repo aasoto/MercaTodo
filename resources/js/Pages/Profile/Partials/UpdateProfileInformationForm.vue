8a8b4c431a569b602b0ae1adec95d879cf94e2dd
aasoto <andresalfredosotosuarez@gmail.com>
2023-03-24 14:45:12 -0500

    feat: Profile views and controller adjusted to new database ffields

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -4,11 +4,14 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Link, useForm, usePage } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
 defineProps({
+    cities: Array,
     mustVerifyEmail: {
         type: Boolean,
     },
+    states: Array,
     status: {
         type: String,
     },
@@ -17,9 +20,29 @@ defineProps({
 const user = usePage().props.auth.user;
 
 const form = useForm({
-    name: user.name,
+    firstName: user.first_name,
+    secondName: user.second_name,
+    surname: user.surname,
+    secondSurname: user.second_surname,
     email: user.email,
+    birthdate: user.birthdate,
+    gender: user.gender,
+    phone: user.phone,
+    address: user.address,
+    state: user.state_id,
+    city: user.city_id,
 });
+
+const state_selected = ref(form.state);
+
+const show_cities = (stateId) => {
+    form.city = '';
+    if (stateId) {
+        state_selected.value = stateId;
+    } else {
+        state_selected.value = 0;
+    }
+}
 </script>
 
 <template>
@@ -33,35 +56,192 @@ const form = useForm({
         </header>
 
         <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
-            <div>
-                <InputLabel for="name" value="Name" />
+            <div class="grid grid-cols-2 gap-5">
+                <div class="col-span-1">
+                    <InputLabel for="firstName" value="Primer nombre" />
 
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+                    <TextInput
+                        id="firstName"
+                        type="text"
+                        class="mt-1 block w-full"
+                        v-model="form.firstName"
+                        required
+                        autofocus
+                        autocomplete="firstName"
+                    />
 
-                <InputError class="mt-2" :message="form.errors.name" />
-            </div>
+                    <InputError class="mt-2" :message="form.errors.firstName" />
+                </div>
 
-            <div>
-                <InputLabel for="email" value="Email" />
+                <div class="col-span-1">
+                    <InputLabel for="secondName" value="Segundo nombre" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
+                    <TextInput
+                        id="secondName"
+                        type="text"
+                        class="mt-1 block w-full"
+                        v-model="form.secondName"
+                        autocomplete="secondName"
+                    />
 
-                <InputError class="mt-2" :message="form.errors.email" />
+                    <InputError class="mt-2" :message="form.errors.secondName" />
+                </div>
+
+                <div class="col-span-1">
+                    <InputLabel for="surname" value="Primer apellido" />
+
+                    <TextInput
+                        id="surname"
+                        type="text"
+                        class="mt-1 block w-full"
+                        v-model="form.surname"
+                        autocomplete="surname"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.surname" />
+                </div>
+
+                <div class="col-span-1">
+                    <InputLabel for="secondSurname" value="Segundo apellido" />
+
+                    <TextInput
+                        id="secondSurname"
+                        type="text"
+                        class="mt-1 block w-full"
+                        v-model="form.secondSurname"
+                        autocomplete="secondSurname"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.secondSurname" />
+                </div>
+
+                <div class="col-span-1">
+                    <InputLabel for="gender" value="Genero" />
+
+
+                    <select
+                        class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                        name="gender"
+                        id="gender"
+                        v-model="form.gender"
+                        required
+                    >
+                        <option value="">Seleccionar...</option>
+                        <option value="m">Masculino</option>
+                        <option value="f">Femenino</option>
+                        <option value="o">Otro</option>
+                    </select>
+
+                    <InputError class="mt-2" :message="form.errors.gender" />
+                </div>
+
+                <div class="col-span-1">
+                    <InputLabel for="birthdate" value="Fecha de nacimiento" />
+
+                    <TextInput
+                        id="birthdate"
+                        type="date"
+                        class="mt-1 block w-full"
+                        v-model="form.birthdate"
+                        required
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.birthdate" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="address" value="Dirección" />
+
+                    <TextInput
+                        id="address"
+                        type="text"
+                        class="mt-1 block w-full"
+                        v-model="form.address"
+                        required
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.address" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="phone" value="Telefono o celular" />
+
+                    <TextInput
+                        id="phone"
+                        type="text"
+                        class="mt-1 block w-full"
+                        v-model="form.phone"
+                        required
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.phone" />
+                </div>
+
+                <div class="mt-4">
+                    <InputLabel for="state" value="Estado" />
+
+                    <select
+                        class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                        name="state"
+                        id="state"
+                        v-model="form.state"
+                        @change="show_cities(form.state)"
+                        required
+                    >
+                        <option value="">Seleccionar...</option>
+                        <option
+                            v-for="state in states"
+                            :value="state.id"
+                            :key="state.id"
+                        >
+                            {{ state.name }}
+                        </option>
+                    </select>
+
+                    <InputError class="mt-2" :message="form.errors.state" />
+
+                </div>
+
+                <div class="mt-4" v-if="state_selected !== 0">
+                    <InputLabel for="city" value="Ciudad" />
+
+                    <select
+                        class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                        name="city"
+                        id="city"
+                        v-model="form.city"
+                        required
+                    >
+                        <option value="">Seleccionar...</option>
+                        <template v-for="city in cities">
+                            <option
+                                v-if="city.state_id == state_selected"
+                                :value="city.id"
+                                :key="city.id"
+                            >
+                                {{ city.name }}
+                            </option>
+                        </template>
+                    </select>
+
+                    <InputError class="mt-2" :message="form.errors.city" />
+
+                </div>
+
+                <div class="col-span-1">
+                    <InputLabel for="email" value="Email" />
+
+                    <TextInput
+                        id="email"
+                        type="email"
+                        class="mt-1 block w-full"
+                        v-model="form.email"
+                        required
+                        autocomplete="username"
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
             </div>
 
             <div v-if="mustVerifyEmail && user.email_verified_at === null">
