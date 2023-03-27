342d007e44aaef73b5bcf276424d3e049d5c2f40
aasoto <andresalfredosotosuarez@gmail.com>
2023-03-26 22:29:57 -0500

    style: link usuarios for small displays

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -125,6 +125,11 @@ defineProps({
                             Dashboard
                         </ResponsiveNavLink>
                     </div>
+                    <div class="pt-2 pb-3 space-y-1">
+                        <ResponsiveNavLink :href="route('user.index')" :active="route().current('user.index')">
+                            Usuarios
+                        </ResponsiveNavLink>
+                    </div>
 
                     <!-- Responsive Settings Options -->
                     <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
