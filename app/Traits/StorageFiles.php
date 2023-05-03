f5a0a2d86328a109e1dbecbd8420c6070a797822
aasoto <andresalfredosotosuarez@gmail.com>
2023-05-02 22:54:57 -0500

    feat: Create log for files

--- app/Traits/StorageFiles.php
+++ app/Traits/StorageFiles.php
@@ -2,6 +2,7 @@
 namespace App\Traits;
 
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 /**
  *
@@ -13,12 +14,16 @@ trait StorageFiles
         $filename = time().$counter.'.'.$file->extension();
         $file->storeAs($path, $filename, 'public');
 
+        Log::channel('files')->info('Add file in path '.$path.' with the name '.$filename);
+
         return $filename;
     }
 
     public function remove(string $path, string $file): void
     {
-        // unlink(storage_path($path.$file));
         Storage::disk('public')->delete($path.'/'.$file);
+
+        Log::channel('files')->warning('Delete file '.$path.'/'.$file);
+
     }
 }
