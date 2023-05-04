da816ce42aa224f864e39ddcfa4ac5cf7be87be6
aasoto <andresalfredosotosuarez@gmail.com>
2023-05-03 23:12:28 -0500

    feat: Create register users actions

---
+++ app/Actions/Register/StoreRegisterAction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Actions\Register;
+
+use App\Http\Requests\Auth\RegisteredUser\StoreRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class StoreRegisterAction
+{
+    public function handle(StoreRequest $request): User
+    {
+        $data = $request->validated();
+
+        return User::create([
+            'type_document' => $data['typeDocument'],
+            'number_document' => $data['numberDocument'],
+            'first_name' => $data['firstName'],
+            'second_name' => $data['secondName'],
+            'surname' => $data['surname'],
+            'second_surname' => $data['secondSurname'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'birthdate' => $data['birthdate'],
+            'gender' => $data['gender'],
+            'phone' => $data['phone'],
+            'address' => $data['address'],
+            'state_id' => $data['state'],
+            'city_id' => $data['city']
+        ])->assignRole('client');
+    }
+}
