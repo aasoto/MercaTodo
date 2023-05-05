f437754e31fecb623c7dfeeb06d9710244bf3c75
aasoto <andresalfredosotosuarez@gmail.com>
2023-05-05 15:42:35 -0500

    feat: Format cache in logout not allowed

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -49,7 +49,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        Cache::flush();
+        // Cache::flush();
 
         return redirect('/');
     }
