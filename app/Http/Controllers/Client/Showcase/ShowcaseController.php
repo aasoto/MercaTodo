89e494a5854fd39f1cf8b6a95efaf1620cb26c1b
aasoto <andresalfredosotosuarez@gmail.com>
2023-05-05 15:13:26 -0500

    test: Edit showcase tests for full coverage

--- app/Http/Controllers/Client/Showcase/ShowcaseController.php
+++ app/Http/Controllers/Client/Showcase/ShowcaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Client\Showcase;
 
+use App\Classes\Showcase\Action;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Traits\AuthHasRole;
@@ -14,31 +15,11 @@ class ShowcaseController extends Controller
 {
     use AuthHasRole, useCache;
 
-    public function index(Request $request): Response
+    public function index(Request $request, Action $showcase): Response
     {
         return Inertia::render('Showcase/Index', [
             'filters' => $request->only(['search', 'category']),
-            'products' => Product::query()
-                -> when($request->input('search'), function ($query, $search) {
-                    $query->where('products.name', 'like', '%'.$search.'%');
-                })
-                -> when($request->input('category'), function ($query, $category) {
-                    $query->where('products_categories.name', $category);
-                })
-                -> select(
-                        'products.name',
-                        'products.slug',
-                        'products_categories.name as category',
-                        'products.price',
-                        'units.name as unit',
-                        'products.picture_1'
-                    )
-                -> join('products_categories', 'products.products_category_id', 'products_categories.id')
-                -> join('units', 'products.unit', 'units.code')
-                -> orderBy('products.id')
-                -> where('availability', 1)
-                -> paginate(12)
-                -> withQueryString(),
+            'products' => $showcase->index($request),
             'products_categories' => $this->getProductsCategories(),
             'userRole' => session('user_role') ? session('user_role') : $this->authHasRole($this->getRoles()),
         ]);
