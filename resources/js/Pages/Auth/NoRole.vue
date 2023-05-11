9a06b42dd7fa04710f259bc6de88a5cd925d2a96
aasoto <andresalfredosotosuarez@gmail.com>
2023-05-10 23:08:10 -0500

    refactor: Change StartController for a middleware called Start

---
+++ resources/js/Pages/Auth/NoRole.vue
@@ -0,0 +1,26 @@
+<script setup>
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Link } from '@inertiajs/vue3';
+
+</script>
+<template>
+    <GuestLayout>
+        <Head title="Usuario inhabilitado" />
+
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+            Este usuario no tiene un rol asignado o cuenta con un tipo de rol no soportado por el sistema, comuniquese con los administradores.
+        </div>
+
+        <Link
+            :href="route('logout')"
+            method="post"
+        >
+            <div class="mt-4 flex items-center justify-center">
+                <PrimaryButton>
+                    Cerrar sesión
+                </PrimaryButton>
+            </div>
+        </Link>
+    </GuestLayout>
+</template>
