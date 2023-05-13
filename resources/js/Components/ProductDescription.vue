62e374872f17987dddc3d66dc7933a8f4e49b50b
aasoto <andresalfredosotosuarez@gmail.com>
2023-05-13 12:24:57 -0500

    feat: Validate the persistence of quantity by product and when one this is removed can be added again including the quantity only when the user stayed in the page of the product

--- resources/js/Components/ProductDescription.vue
+++ resources/js/Components/ProductDescription.vue
@@ -52,8 +52,14 @@ const errorInfo = ref('');
 
 const { add, find, update, remove } = useCart;
 
+let found = find(props.product.id);
+
+if (found?.quantity) {
+    quantity.value = found.quantity;
+}
+
 const actionAdd = () => {
-    const found = find(props.product.id);
+    found = find(props.product.id);
     found ? updateCart() : addToCart();
 }
 
@@ -83,6 +89,11 @@ const updateCart = () => {
         errorAlert.value = true;
         errorInfo.value = error;
     }
+}
+
+const removeFromCart = () => {
+    remove(props.product.id);
+    found = find(props.product.id);
 }
 
 </script>
@@ -182,7 +193,7 @@ const updateCart = () => {
                                     <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
                                 </svg>
                             </button>
-                            <button v-if="find(product.id)" @click="remove(product.id)" class="rounded-md px-5 py-3 bg-gray-200 text-gray-800 scale-100 hover:scale-105 transition duration-200">
+                            <button v-if="find(product.id)" @click="removeFromCart()" class="rounded-md px-5 py-3 bg-gray-200 text-gray-800 scale-100 hover:scale-105 transition duration-200">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                 </svg>
