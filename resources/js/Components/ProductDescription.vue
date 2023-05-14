6cd383ed07e7b4e3c70f29bf4f5a717df2e0442d
aasoto <andresalfredosotosuarez@gmail.com>
2023-05-13 20:59:33 -0500

    feat: Change color of add to the cart button when the product has been added

--- resources/js/Components/ProductDescription.vue
+++ resources/js/Components/ProductDescription.vue
@@ -185,9 +185,18 @@ const removeFromCart = () => {
                                     </button>
                                 </div>
                             </div>
-                            <button @click="actionAdd()" class="flex justify-center items-center gap-2 bg-red-500 hover:bg-red-600 scale-100 hover:scale-105 text-white px-5 py-3 rounded-md transition duration-200">
+                            <button
+                                @click="actionAdd()"
+                                class="flex justify-center items-center gap-2 scale-100 hover:scale-105 px-5 py-3 rounded-md transition duration-200"
+                                :class="find(product.id) ? 'bg-yellow-300 hover:bg-yellow-400 text-black' : 'bg-red-500 hover:bg-red-600 text-white'"
+                            >
                                 <span>
-                                    Añadir al carrito
+                                    <template v-if="find(product.id)">
+                                        Actualizar cantidad
+                                    </template>
+                                    <template v-else>
+                                        Añadir al carrito
+                                    </template>
                                 </span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                                     <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
