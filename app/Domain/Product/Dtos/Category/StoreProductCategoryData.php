03fb6b0b65feae3e005b5430f7f060c9665d6397
aasoto <andresalfredosotosuarez@gmail.com>
2023-05-21 17:37:56 -0500

    refactor: Apply phpstan suggestions products categories

--- app/Domain/Product/Dtos/Category/StoreProductCategoryData.php
+++ app/Domain/Product/Dtos/Category/StoreProductCategoryData.php
@@ -4,6 +4,7 @@ namespace App\Domain\Product\Dtos\Category;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+/** @phpstan-consistent-constructor */
 class StoreProductCategoryData
 {
     public function __construct(
