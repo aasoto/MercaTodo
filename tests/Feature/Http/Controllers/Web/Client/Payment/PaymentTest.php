209c6833984097f895fbe89b7c76f9177d03470d
aasoto <andresalfredosotosuarez@gmail.com>
2023-06-04 22:19:17 -0500

    test: Make new tests for differents response from the api

--- tests/Feature/Http/Controllers/Web/Client/Payment/PaymentTest.php
+++ tests/Feature/Http/Controllers/Web/Client/Payment/PaymentTest.php
@@ -67,8 +67,235 @@ class PaymentTest extends TestCase
 
         $this->assertEquals($order->url, '');
         $this->assertEquals($order->request_id, '');
+
         $order = $order->fresh();
+
         $this->assertEquals($order->url, 'https://checkout-co.placetopay.dev/spa/session/1213/1234');
         $this->assertEquals($order->request_id, '1213');
+    }
+
+    public function test_can_regenerate_new_payment_link_in_canceled_order(): void
+    {
+        $this->actingAs($this->user);
+        $order = Order::factory()->create([
+            'user_id' => $this->user->getKey(),
+            'url' => 'https://checkout-co.placetopay.dev/spa/session/0000/0000',
+            'request_id' => 0000,
+            'payment_status' => 'canceled',
+        ]);
+
+        $mock_response = [
+            "status" => [
+                "status" => "OK",
+                "reason" => "PC",
+                "message" => "La petición se ha procesado correctamente",
+                "date" => "2023-06-04T15:07:52-05:00"
+            ],
+            "requestId" => 72994,
+            "processUrl" => "https://checkout-co.placetopay.dev/spa/session/72994/1234",
+        ];
+
+        Http::fake([config('placetopay.url').'/*' => Http::response($mock_response)]);
+
+        $this->patchJson(route('payment.update', $order->getKey()), [
+            'id' => $order->getKey(),
+        ])->assertRedirectContains('order/'.$order->getKey());
+
+        $this->assertEquals($order->url, 'https://checkout-co.placetopay.dev/spa/session/0000/0000');
+        $this->assertEquals($order->request_id, '0000');
+        $this->assertEquals($order->payment_status, 'canceled');
+
+        $order = $order->fresh();
+
+        $this->assertEquals($order->url, 'https://checkout-co.placetopay.dev/spa/session/72994/1234');
+        $this->assertEquals($order->request_id, '72994');
+        $this->assertEquals($order->payment_status, 'pending');
+    }
+
+    public function test_can_pay_order_from_the_webcheckout_platform(): void
+    {
+        $this->actingAs($this->user);
+        $order = Order::factory()->create([
+            'user_id' => $this->user->getKey(),
+            'url' => 'https://checkout-co.placetopay.dev/spa/session/0000/0000',
+            'request_id' => 0000,
+        ]);
+
+        $mock_response = [
+            "requestId" => 0000,
+            "status" => [
+                "status" => "APPROVED",
+                "reason" => "00",
+                "message" => "La petición ha sido aprobada exitosamente",
+                "date" => "2023-06-04T12:04:01-05:00"
+            ]
+        ];
+
+        Http::fake([config('placetopay.url').'/*' => Http::response($mock_response)]);
+
+        $this->getJson(route('payment.response', $order->code))
+            ->assertRedirectContains('order/'.$order->getKey());
+
+        $this->assertEquals($order->payment_status, 'pending');
+
+        $order = $order->fresh();
+
+        $this->assertEquals($order->payment_status, 'paid');
+    }
+
+    public function test_can_cancel_payment_process_from_webcheckout_platform(): void
+    {
+        $this->actingAs($this->user);
+        $order = Order::factory()->create([
+            'user_id' => $this->user->getKey(),
+            'url' => 'https://checkout-co.placetopay.dev/spa/session/0000/0000',
+            'request_id' => 0000,
+        ]);
+
+        $mock_response = [
+            "requestId" => 0000,
+            "status" => [
+                "status" => "REJECTED",
+                "reason" => "?C",
+                "message" => "La petición ha sido cancelada por el usuario",
+                "date" => "2023-06-04T15:06:14-05:00"
+            ]
+        ];
+
+        Http::fake([config('placetopay.url').'/*' => Http::response($mock_response)]);
+
+        $this->getJson(route('payment.response', $order->code))
+            ->assertRedirectContains('order/'.$order->getKey());
+
+        $this->assertEquals($order->payment_status, 'pending');
+
+        $order = $order->fresh();
+
+        $this->assertEquals($order->payment_status, 'canceled');
+    }
+
+    public function test_can_report_transaction_in_process(): void
+    {
+        $this->actingAs($this->user);
+        $order = Order::factory()->create([
+            'user_id' => $this->user->getKey(),
+            'url' => 'https://checkout-co.placetopay.dev/spa/session/0000/0000',
+            'request_id' => 0000,
+        ]);
+
+        $mock_response = [
+            "requestId" => 0000,
+            "status" => [
+                "status" => "PENDING",
+                "reason" => "PE",
+                "message" => "La petición se encuentra pendiente",
+                "date" => "2023-06-04T14:57:16-05:00"
+            ]
+        ];
+
+        Http::fake([config('placetopay.url').'/*' => Http::response($mock_response)]);
+
+        $this->getJson(route('payment.response', $order->code))
+            ->assertRedirectContains('order/'.$order->getKey());
+
+        $this->assertEquals($order->payment_status, 'pending');
+
+        $order = $order->fresh();
+
+        $this->assertEquals($order->payment_status, 'waiting');
+    }
+
+    public function test_can_verify_if_the_session_is_stil_active(): void
+    {
+        $this->actingAs($this->user);
+        $order = Order::factory()->create([
+            'user_id' => $this->user->getKey(),
+            'url' => 'https://checkout-co.placetopay.dev/spa/session/0000/0000',
+            'request_id' => 0000,
+        ]);
+
+        $mock_response = [
+            "requestId" => 0000,
+            "status" => [
+                "status" => "PENDING",
+                "reason" => "PC",
+                "message" => "La petición se encuentra activa",
+                "date" => "2023-06-04T14:58:07-05:00"
+            ]
+        ];
+
+        Http::fake([config('placetopay.url').'/*' => Http::response($mock_response)]);
+
+        $this->getJson(route('payment.response', $order->code))
+            ->assertRedirectContains('order/'.$order->getKey());
+
+        $this->assertEquals($order->payment_status, 'pending');
+
+        $order = $order->fresh();
+
+        $this->assertEquals($order->payment_status, 'pending');
+    }
+
+    public function test_can_change_payment_status_when_the_webcheckout_link_has_expired(): void
+    {
+        $this->actingAs($this->user);
+        $order = Order::factory()->create([
+            'user_id' => $this->user->getKey(),
+            'url' => 'https://checkout-co.placetopay.dev/spa/session/0000/0000',
+            'request_id' => 0000,
+        ]);
+
+        $mock_response = [
+            "requestId" => 0000,
+            "status" => [
+                "status" => "REJECTED",
+                "reason" => "EX",
+                "message" => "La petición ha expirado",
+                "date" => "2023-06-04T11:48:41-05:00"
+            ]
+        ];
+
+        Http::fake([config('placetopay.url').'/*' => Http::response($mock_response)]);
+
+        $this->getJson(route('payment.response', $order->code))
+            ->assertRedirectContains('order/'.$order->getKey());
+
+        $this->assertEquals($order->payment_status, 'pending');
+
+        $order = $order->fresh();
+
+        $this->assertEquals($order->payment_status, 'canceled');
+    }
+
+    public function test_can_change_payment_status_when_client_has_been_redirect_from_canceled_link(): void
+    {
+        $this->actingAs($this->user);
+        $order = Order::factory()->create([
+            'user_id' => $this->user->getKey(),
+            'url' => 'https://checkout-co.placetopay.dev/spa/session/0000/0000',
+            'request_id' => 0000,
+        ]);
+
+        $mock_response = [
+            "requestId" => 0000,
+            "status" => [
+                "status" => "REJECTED",
+                "reason" => "?C",
+                "message" => "La petición ha sido cancelada por el usuario",
+                "date" => "2023-06-04T22:01:16-05:00"
+            ]
+        ];
+
+        Http::fake([config('placetopay.url').'/*' => Http::response($mock_response)]);
+
+        $this->getJson(route('payment.canceled', $order->code))
+            ->assertRedirect(route('showcase.index'))
+            ->assertSessionHasAll(['success' => 'Payment canceled.']);
+
+        $this->assertEquals($order->payment_status, 'pending');
+
+        $order = $order->fresh();
+
+        $this->assertEquals($order->payment_status, 'canceled');
     }
 }
