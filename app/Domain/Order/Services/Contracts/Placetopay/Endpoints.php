8d49b116a7afaef2e32a09f5e3d2fda4cfb27e43
aasoto <andresalfredosotosuarez@gmail.com>
2023-06-19 11:27:08 -0500

    refactor: Change api endpoint, delete environment varible and create interface with constant

---
+++ app/Domain/Order/Services/Contracts/Placetopay/Endpoints.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Domain\Order\Services\Contracts\Placetopay;
+
+interface Endpoints
+{
+    public const API_SESSION = '/api/session/';
+}
--- config/placetopay.php
+++ config/placetopay.php
@@ -4,7 +4,4 @@ return [
     'login' => env('PLACETOPAY_LOGIN'),
     'tranKey' => env('PLACETOPAY_TRANKEY'),
     'url' => env('PLACETOPAY_URL'),
-    'route' => [
-        'api' => env('PLACETOPAY_API'),
-    ]
 ];
