37b3f4270ca7ab55e5ea113bfbaebf99fd776ed2
aasoto <andresalfredosotosuarez@gmail.com>
2023-06-22 15:26:24 -0500

    style: Add MulticolorInfo component

--- resources/js/Pages/Order/Partials/PaymentStatus.vue
+++ resources/js/Pages/Order/Partials/PaymentStatus.vue
@@ -1,55 +1,33 @@
 <script setup>
+import MulticolorInfo from '@/Components/Infos/MulticolorInfo.vue';
+
 const props = defineProps({
     order: Object,
 });
 </script>
 <template>
-    <div
-        v-if="order.payment_status == 'canceled'"
-        class="rounded-md px-2 py-2 bg-red-200 text-center font-bold"
-    >
+    <MulticolorInfo color="red" v-if="order.payment_status == 'canceled'">
         CANCELADO
-    </div>
-    <div
-        v-if="order.payment_status == 'paid'"
-        class="rounded-md px-2 py-2 bg-green-200 text-center font-bold"
-    >
+    </MulticolorInfo>
+    <MulticolorInfo color="green" v-if="order.payment_status == 'paid'">
         PAGADO
-    </div>
-    <div
-        v-if="order.payment_status == 'waiting'"
-        class="rounded-md px-2 py-2 bg-purple-300 text-center font-bold"
-    >
+    </MulticolorInfo>
+    <MulticolorInfo color="purple" v-if="order.payment_status == 'waiting'">
         PAGO POR CONFIRMAR
-    </div>
-    <div
-        v-if="order.url && (order.payment_status == 'pending')"
-        class="rounded-md px-2 py-2 bg-yellow-200 text-center font-bold"
-    >
+    </MulticolorInfo>
+    <MulticolorInfo color="yellow" v-if="order.url && (order.payment_status == 'pending')">
         PENDIENTE
-    </div>
-    <div
-        v-if="order.payment_status == 'verify_bank'"
-        class="rounded-md px-2 py-2 bg-orange-300 text-center font-bold"
-    >
+    </MulticolorInfo>
+    <MulticolorInfo color="orange" v-if="order.payment_status == 'verify_bank'">
         VERIFICAR CON SU BANCO
-    </div>
-    <div
-        v-if="order.payment_status == 'approved_partial'"
-        class="rounded-md px-2 py-2 bg-amber-300 text-center font-bold"
-    >
+    </MulticolorInfo>
+    <MulticolorInfo color="amber" v-if="order.payment_status == 'approved_partial'">
         PAGO INCOMPLETO
-    </div>
-    <div
-        v-if="order.payment_status == 'partial_expired'"
-        class="rounded-md px-2 py-2 bg-zinc-300 text-center font-bold"
-    >
+    </MulticolorInfo>
+    <MulticolorInfo color="zinc" v-if="order.payment_status == 'partial_expired'">
         PAGO EXPIRADO
-    </div>
-    <div
-        v-if="!order.url"
-        class="rounded-md px-2 py-2 bg-blue-200 text-center font-bold"
-    >
+    </MulticolorInfo>
+    <MulticolorInfo color="blue" v-if="!order.url">
         SIN LINK DE PAGO
-    </div>
+    </MulticolorInfo>
 </template>
