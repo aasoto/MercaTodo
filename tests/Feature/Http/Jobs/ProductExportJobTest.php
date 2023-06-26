6645934f6b7d1cccee0aba505214d63bf9d2aa29
aasoto <andresalfredosotosuarez@gmail.com>
2023-06-26 16:56:23 -0500

    test: Modify product export job test for send email with export products file .csv

--- tests/Feature/Http/Jobs/ProductExportJobTest.php
+++ tests/Feature/Http/Jobs/ProductExportJobTest.php
@@ -3,11 +3,19 @@
 namespace Tests\Feature\Http\Jobs;
 
 use App\Domain\Product\Models\Product;
+use App\Domain\User\Models\User;
 use App\Http\Jobs\ProductExportJob;
+use App\Http\Mail\SendEmailExportProducts;
+use Database\Seeders\CitySeeder;
 use Database\Seeders\ProductCategorySeeder;
+use Database\Seeders\RoleSeeder;
+use Database\Seeders\StateSeeder;
+use Database\Seeders\TypeDocumentSeeder;
 use Database\Seeders\UnitSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Tests\TestCase;
@@ -20,15 +28,27 @@ class ProductExportJobTest extends TestCase
     {
         Storage::fake(config()->get('filesystem.default'));
         Storage::fake('public');
+        Mail::fake();
 
         $this->seed([
+            StateSeeder::class,
+            CitySeeder::class,
+            TypeDocumentSeeder::class,
             ProductCategorySeeder::class,
             UnitSeeder::class,
+            RoleSeeder::class,
         ]);
 
         Product::factory()->count(4)->create();
+
         $uuid = Str::uuid()->serialize();
-        (new ProductExportJob($uuid))->handle();
+
+        (new ProductExportJob(
+            User::factory()->create()->assignRole('admin'),
+            $uuid)
+        )->handle();
+
         Storage::disk(config()->get('filesystem.default'))->assertExists('exports/'.$uuid.'.csv');
+        Mail::assertSent(SendEmailExportProducts::class);
     }
 }
