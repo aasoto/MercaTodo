54eaae8b39f340b5fcc33a42aec5184245e0c5a7
aasoto <andresalfredosotosuarez@gmail.com>
2023-07-02 18:28:17 -0500

    test: Create test for import products

--- tests/Feature/Http/Controllers/Web/Admin/Product/ProductImportControllerTest.php
+++ tests/Feature/Http/Controllers/Web/Admin/Product/ProductImportControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers\Api\Admin;
+namespace Tests\Feature\Http\Controllers\Web\Admin\Product;
 
 use App\Domain\User\Models\User;
 use App\Http\Jobs\ProductImportJob;
@@ -12,7 +12,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Queue;
-use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class ProductImportControllerTest extends TestCase
@@ -35,18 +34,16 @@ class ProductImportControllerTest extends TestCase
         $this->user = User::factory()->create()->assignRole('admin');
     }
 
-    public function test_can_enqueue_products_import_job_from_api(): void
+    public function test_can_enqueue_products_import_job(): void
     {
-        Sanctum::actingAs($this->user);
         Queue::fake();
 
-        $response = $this->post(route('api.product.import'), [
-            'products_file' => new UploadedFile(__DIR__.'/../../../../../Support/import-file.csv', 'testing_file', test: true),
-        ]);
-
-        $response
-            ->assertStatus(201)
-            ->assertJsonStructure(['message', 'file']);
+        $this->actingAs($this->user)->post(route('product.import.store'), [
+            'products_file' => new UploadedFile(__DIR__.'/../../../../../../Support/import-file.csv', 'testing_file', test: true),
+        ])
+        ->assertRedirect()
+        ->assertRedirect(route('products.index'))
+        ->assertSessionHasAll(['success' => 'Products imported.']);
 
         Queue::assertPushed(ProductImportJob::class);
     }
