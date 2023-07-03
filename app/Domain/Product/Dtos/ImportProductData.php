45337e34cd0397f31459aee2f1800171847346b4
aasoto <andresalfredosotosuarez@gmail.com>
2023-07-03 10:39:42 -0500

    feat: Add default disk in storage import file

--- app/Domain/Product/Dtos/ImportProductData.php
+++ app/Domain/Product/Dtos/ImportProductData.php
@@ -3,7 +3,7 @@
 namespace App\Domain\Product\Dtos;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 /** @phpstan-consistent-constructor */
 class ImportProductData
@@ -16,7 +16,7 @@ class ImportProductData
     public static function fromRequest(FormRequest $request): self
     {
         return new static(
-            products_file: $request->file('products_file')->store('imports'),
+            products_file: Storage::disk(config()->get('filesystem.default'))->put('imports', $request->file('products_file')),
         );
     }
 }
